9ba950d9cbaa4a91bc716f55e96403e7483a2a4e
kabballel <63969391+kaballel@users.noreply.github.com>
2022-03-13 14:08:56 -0700

    Update VAR_regression_script.py

--- VAR_regression_script.py
+++ VAR_regression_script.py
@@ -7,5 +7,9 @@ from statsmodels.tsa.api import VAR
 from statsmodels.tsa.stattools import adfuller
 from statsmodels.tools.eval_measures import rmse, aic
 
+# Variable Names for Reference:
+# pop, prodElectric, lenRail, agrProd, numPatents, numMiners, numCorps, percAgr, percManu, gdpPerCap
+
+
 # Import Data for Analysis
 pd.read_csv('./Primary_Dataset.csv')
