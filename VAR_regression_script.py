e8e09ece64675c0ce5c0d1e38fc4d4225352cb71
kabballel <63969391+kaballel@users.noreply.github.com>
2022-03-13 13:57:36 -0700

    Add Initial Libraries

---
+++ VAR_regression_script.py
@@ -0,0 +1,11 @@
+# Import Libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from statsmodels.tsa.api import VAR
+from statsmodels.tsa.stattools import adfuller
+from statsmodels.tools.eval_measures import rmse, aic
+
+# Import Data for Analysis
+filepath =
